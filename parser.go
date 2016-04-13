4cdf18c6c28f6efeb566ed3c1724060fbe6bb20a
Robert <robert@robertrhu.net>
2016-04-13 10:49:35 -0600

    V2

--- parser.go
+++ parser.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	 "fmt"
+	 "bufio"
+	 "io/ioutil"
+	 "os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	path := "data/elks" //Set the path of the files
+	files := LoadFiles(path)
+	data := ExtractData(path, files)
+	ExportData(data)
+}
+func LoadFiles(path string) []os.FileInfo {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return files
+}
+
+func ExtractData(path string, files []os.FileInfo) string {
+	var data = "nothin"
+
+	// For each os.FileInfo
+	for _, v := range files {
+		// Open
+		filename := path + "/" + v.Name()
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Println(err)
+			return data
+		}
+
+		// For each line
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Println(line)
+		}
+
+		// Close the file
+		file.Close()
+
+	}
+
+	return data
+}
+
+func ExportData(data string) {
+	fmt.Println("Exporting Data")
+	fmt.Println(data)
 }
